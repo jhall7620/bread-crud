e85a2bd6f874c992fe2991bb507e779a97b8f204
jhall7620 <jhall7620@gmail.com>
2023-07-12 19:41:17 -0400

    added edit function

--- controllers/bread.js
+++ controllers/bread.js
@@ -19,12 +19,23 @@ router.get('/:index', (req, res) => {
     // res.send(Bread[index])
 })
 
+//GET bread by index
 router.get('/name/:name', (req, res) => {
     const {name} = req.params
     const bread = Bread.find(b => b.name === name)
     res.send(bread)
 })
 
+//GET edit page
+router.get('/:index/edit', (req, res) => {
+    const {index} = req.params
+    res.render('edit', {
+        bread: Bread[index],
+        index
+    })
+})
+
+//POST create a new bread
 router.post('/', (req, res) => {
     if(req.body.hasGluten === 'on'){
         req.body.hasGluten = true
@@ -38,6 +49,29 @@ router.post('/', (req, res) => {
 
     Bread.push(req.body)
     res.redirect('/breads')
+})
+
+router.delete('/:index', (req, res) => {
+    const {index} = req.params
+    Bread.splice(index, 1)
+    res.status(303).redirect('/breads')
+})
+
+// PUT update a bread by index
+router.put('/:index', (req, res) => {
+    const {index} = req.params
+    if(req.body.hasGluten === 'on'){
+        req.body.hasGluten = true
+    } else{
+        req.body.hasGluten = false
+    }
+
+    if(!req.body.image){
+        req.body.image = 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
+    }
+
+    Bread[index] = req.body
+    res.status(303).redirect('/breads')
 })
 
 router.delete('/:index', (req, res) => {
